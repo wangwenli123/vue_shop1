239c67e968d56acb9294895f9f0ad18cbae2c904
wangwenli123 <12200285+wangwenli1@user.noreply.gitee.com>
2023-02-02 16:31:13 +0800

    完成了分类功能的开发

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -44,8 +44,8 @@ export default {
     return {
       // 这是登录表单的数据绑定对象
       loginForm: {
-        username: 'zs',
-        password: '123'
+        username: 'admin',
+        password: '123456'
       },
       // 这是表单的验证规则对象
       loginFormRules: {
@@ -65,20 +65,20 @@ export default {
   methods: {
     // 点击重置按钮，重置登录表单
     resetLoginForm() {
-      // console.log(this)
+      // console.log(this);
       this.$refs.loginFormRef.resetFields()
     },
     login() {
       this.$refs.loginFormRef.validate(async (valid) => {
         if (!valid) return
         const { data: res } = await this.$http.post('login', this.loginForm)
-        if (res.meta.status !== 200) return this.$message.error('登录失败!')
+        if (res.meta.status !== 200) return this.$message.error('登录失败！')
         this.$message.success('登录成功')
-        // 1.将登录成功之后的token 保存到客户端的sessionStorage中
-        // 1.1项目中除了登录之外的其他API接口 必须在登录之后才能访问
-        // 1.2token只应在当前网站打开期间生效 所以将token保存在sessionStorage中
+        // 1. 将登录成功之后的 token，保存到客户端的 sessionStorage 中
+        //   1.1 项目中出了登录之外的其他API接口，必须在登录之后才能访问
+        //   1.2 token 只应在当前网站打开期间生效，所以将 token 保存在 sessionStorage 中
         window.sessionStorage.setItem('token', res.data.token)
-        // 2 通过编程式导航跳转到后台主页 路由地址是/home
+        // 2. 通过编程式导航跳转到后台主页，路由地址是 /home
         this.$router.push('/home')
       })
     }
@@ -91,6 +91,7 @@ export default {
   background-color: #2b4b6b;
   height: 100%;
 }
+
 .login_box {
   width: 450px;
   height: 300px;
@@ -99,8 +100,8 @@ export default {
   position: absolute;
   left: 50%;
   top: 50%;
-  // 第一个-50%是x轴向左移动150px 第二个是向上移动高度的一半
   transform: translate(-50%, -50%);
+
   .avatar_box {
     height: 130px;
     width: 130px;
@@ -108,7 +109,6 @@ export default {
     border-radius: 50%;
     padding: 10px;
     box-shadow: 0 0 10px #ddd;
-    // 图像部分居中位置
     position: absolute;
     left: 50%;
     transform: translate(-50%, -50%);
@@ -120,16 +120,19 @@ export default {
       background-color: #eee;
     }
   }
-  .login_form {
-    position: absolute;
-    bottom: 0;
-    width: 100%;
-    padding: 0 20px;
-    box-sizing: border-box;
-  }
 }
+
+.login_form {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+
 .btns {
   display: flex;
   justify-content: flex-end;
 }
 </style>
+ss
--- src/main.js
+++ src/main.js
@@ -6,10 +6,11 @@ import './plugins/element.js'
 import '@/assets/fonts/iconfont.css'
 // 导入全局样式表
 import '@/assets/css/global.css'
+import TreeTable from 'vue-table-with-tree-grid'
 // 挂载axios
 import axios from 'axios'
 // 配置请求的根路径
-axios.defaults.baseURL = 'https://lianghj.top:8888/api/private/v1/'
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 axios.interceptors.request.use((config) => {
   console.log(config)
   config.headers.Authorization = window.sessionStorage.getItem('token')
@@ -19,6 +20,8 @@ axios.interceptors.request.use((config) => {
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
+
+Vue.component('tree-table', TreeTable)
 
 new Vue({
   router,
